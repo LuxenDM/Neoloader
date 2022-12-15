b1ac72ad60bc1d1366b490ae72ca8b226cd83e74
Luxen De'Mark <Luxen@gmx.com>
2022-12-14 22:09:04 -0500

    File made unneccessary by commit a60f602 2022-12-12

--- Neoloader/empty_mod.lua
+++
@@ -1,5 +0,0 @@
---[[
-If a plugin or library's ini declaration file doesn't have a file path, this file is referenced instead. It doesn't do anything other than print stuff for plugin developers.
-]]--
-
-lib.log_error("A plugin's declaration didn't have a path included!")
