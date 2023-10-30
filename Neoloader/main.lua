63745094817bc80c4ea6f9ad6d4a7e7641880f65
Luxen De'Mark <Luxen@gmx.com>
2023-10-30 01:43:14 -0400

    Redesigned pre-setup system for main.lua. no more automatic installation, which could confuse pilots. Hardcoded EN/ES/FR/PT support.

--- Neoloader/main.lua
+++ Neoloader/main.lua
@@ -1,35 +1,128 @@
-local function cp(...)
+local cp = function(...)
 	console_print(...)
 	print(...)
 end
 
-local function newloader()
-	--Set Neoloader to run as an IF replacement
-	UnregisterUserCommand("neosetup")
-	cp("Neoloader appears to be setting up for the first time")
-	cp("VO will restart with Neoloader momentarily...")
-	local prev_if = gkini.ReadString("Vendetta", "if", "")
-	if prev_if ~= "" and prev_if ~= "vo/if.lua" then
-		cp("A custom interface was already being used! This has been backed up to ['Vendetta', 'if2']")
-		gkini.WriteString("Vendetta", "if2", prev_if)
+local first_time = gkini.ReadString("Neoloader", "first_time", "YES")
+--[[
+	YES: First time ever running Neoloader Main.lua
+	Removed: Neoloader was removed
+	Mute: Don't pester the user about installing
+]]--
+
+if type(lib) == "table" and lib[0] == "LME" then
+	local run_command = gkini.ReadString("Neoloader", "run_command", "")
+	gkini.WriteString("Neoloader", "run_command", "")
+	if run_command ~= "" then
+		lib.log_error("Executing startup run command: " .. run_command)
+		gkinterface.GKProcessCommand(run_command)
 	end
-	gkini.WriteString("Vendetta", "if", "plugins/Neoloader/init.lua")
-	gkini.WriteInt("Neoloader", "Init", 0)
-	RegisterEvent(ReloadInterface, "START")--if someone starts the game, reloads are ignored until this event is fired, so we reg one here.
-	RegisterEvent(ReloadInterface, "PLUGINS_LOADED")--if someone /reloads, this is handled
-	ReloadInterface() --in case its run by /neosetup
+	
+	return
 end
 
-if type(lib) ~= "table" or type(lib[1]) ~= "string" then
-	local prev_if = gkini.ReadString("Vendetta", "if", "")
-	if gkini.ReadString("Neoloader", "uninstalled", "NO") == "NO" and gkini.ReadInt("Vendetta", "Init", 0) == 0 then
-		newloader()
-	elseif prev_if ~= "vo/if.lua" and prev_if ~= "" then
-		cp("You are running a custom interface! Neoloader installation must be manually triggered with /neosetup")
-		cp("Run the setup, and then make your own configurations afterwards. Your IF will be saved as if2 in the config.")
-		RegisterUserCommand("neosetup", newloader)
+----------------------------------------------------------------------------
+--		below this only runs if an LME doesn't exist
+----------------------------------------------------------------------------
+
+local lang_code = gkini.ReadString("Vendetta", "locale", "en")
+local tprint = function(inikey, default)
+	return tprint(inikey, default, "plugins/Neoloader/lang/main.ini")
+end
+
+local action_button = iup.stationbutton {
+	title = "Install Neoloader",
+	select_val = 1,
+	action = function(self)
+		if self.select_val == 1 then
+			dofile("plugins/Neoloader/setup.lua")
+		elseif self.select_val == 2 then
+			print(tprint("delay", "Neoloader setup has been delayed; you can use /neo to open this dialog again"))
+		elseif self.select_val == 3 then
+			gkini.WriteString("Neoloader", "first_time", "MUTE")
+			print(tprint("muted", "Dialog will no longer auto-appear"))
+		end
+		
+		HideDialog(iup.GetDialog(self))
+	end,
+}
+
+local request_setup = function(delay_once)
+	local diag = iup.dialog {
+		topmost = "YES",
+		fullscreen = "YES",
+		bgcolor = "0 0 0 100 *",
+		iup.vbox {
+			iup.fill { },
+			iup.hbox {
+				iup.fill { },
+				iup.stationsubframe {
+					iup.vbox {
+						alignment = "ACENTER",
+						iup.fill {
+							size = Font.Default,
+						},
+						iup.label {
+							title = tprint("request", "Would you like to install Neoloader?"),
+						},
+						iup.fill {
+							size = Font.Default,
+						},
+						iup.hbox {
+							iup.fill {
+								size = Font.Default,
+							},
+							iup.stationsublist {
+								dropdown = "YES",
+								action = function(self, t, i, cv)
+									if cv == 1 then
+										action_button.title = t
+										action_button.select_val = i
+									end
+								end,
+								tprint("list_install", "Install Neoloader"),
+								tprint("list_delay", "Not right now"),
+								tprint("list_mute", "Mute this dialog"),
+							},
+							iup.fill {
+								size = "%5",
+							},
+							action_button,
+							iup.fill {
+								size = Font.Default,
+							},
+						},
+					},
+				},
+				iup.fill { },
+			},
+			iup.fill { },
+		},
+	}
+	
+	diag:map()
+	if delay_once then
+		--making sure it shows up over both game start and /reload events
+		--this is so stupid...
+		local delay_timer = Timer()
+		RegisterEvent(function() 
+			delay_timer:SetTimeout(10, function()
+				ShowDialog(diag)
+			end)
+		end, "PLUGINS_LOADED")
 	else
-		cp("Neoloader appears to have been uninstalled recently, but is still in your game's plugins. If you have installed a new version of Neoloader or wish to run it again, use /neosetup")
-		RegisterUserCommand("neosetup", newloader)
+		ShowDialog(diag)
 	end
 end
+
+if first_time == "YES" then
+	request_setup(true)
+elseif first_time == "Removed" then
+	print(tprint("reins_notice", "Neoloader was recently removed; use /neo to reinstall"))
+elseif first_time == "MUTE" then
+	print(tprint("mute_remind", "Use /neo to start the installation process!"))
+elseif first_time == "SKIP" then
+	cp("Neoloader was supposed to be launched by an external tool.")
+end
+
+RegisterUserCommand("neo", request_setup)
