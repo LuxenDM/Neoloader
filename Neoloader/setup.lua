952b769dcedf1763fee325d786fc7269624b77e8
Luxen De'Mark <81200411+LuxenDM@users.noreply.github.com>
2025-05-10 22:26:23 -0400

    added update_check value to skip patch on new installs

--- Neoloader/setup.lua
+++ Neoloader/setup.lua
@@ -29,6 +29,7 @@ gkwi("Neoloader", "Init", 3)
 gkws("Neoloader", "mgr", "neomgr")
 gkws("Neoloader", "current_notif", "neonotif")
 gkws("Neoloader", "uninstalled", "NO")
+gkwi("Neoloader", "update_check", 1)
 
 local counter = 1
 while true do
