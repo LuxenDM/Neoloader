0e588a08e8da3e60a2b6d80bde884616777a97b9
Luxen De'Mark <81200411+LuxenDM@users.noreply.github.com>
2025-07-16 20:31:14 -0400

    Update recovery.lua

--- Neoloader/recovery.lua
+++ Neoloader/recovery.lua
@@ -12,8 +12,12 @@ rs.error: This is the error string to display when visible
 
 rs.critical: This boolean informs the resolution system that Neoloader has a core issue and cannot run. self-fix options will be reduced to reload, reset Neoloader, reset game, etc.
 
-rs.immediate: This boolean informs the recovery menu display to use popup instead of show behavior. Popup pauses all game execution until the dialog is hidden, but it can also be used during the game's loading screen. very dangerous, used only for self-resolution here.
-
 rs.push_error()
 	This function triggers the recovery menu to display immediately (using popup if critical or immediate booleans are true). non-immediate errors are pushed onto a stack in case multiple errors are being triggered, allowing all to be viewed.
+
+rs.file_check_success()
+	This informs the recovery system (if it has been loaded) that the initial check for files has succeeded, and Neoloader will begin loading. The recovery system recieves the auth key at this time.
+
+rs.init_flag()
+	This flags various 
 ]]--
