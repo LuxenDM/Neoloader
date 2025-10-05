bee3a0d325f5f3c7e4de4951826577528cbba2df
Luxen De'Mark <81200411+LuxenDM@users.noreply.github.com>
2025-10-05 16:59:48 -0400

    Update config.lua

--- Neoloader/modules/config.lua
+++ Neoloader/modules/config.lua
@@ -6,8 +6,15 @@ description=This is the configuration manager for Neoloader
 --[[
 these configuration options have been deprecated:
 
-allow_delayed_load: Now always yes
-list_presorted: New loading system automagically handles load order regardless of input order
+allow_delayed_load: Now always yes: preparing for VFS-based plugin support, which may be downloaded post-game load. NLMPE v7+ should explicitly make loading these seamless without reload.
+list_presorted: New loading system automagically handles load order regardless of input order, no need to support or handle hardcoded load orders.
+echo_logging: logging will always occur now. No need to induce confusion for logs not being added to the game console by user-facing switch.
 
+These configuration options are supported:
+
+override_disabled_plugin_state: YES to load Neoloader even if plugins are disabled by the game client. Default to NO.
+allow_bad_api_version: YES to load plugins that don't match the LME API version of Neoloader. *might* cause bugs. This can also be a per-plugin switch, by using load state of "YES -API"
+default_load_state: YES or NO, are plugins allowed to load when they are first registered. defaults to YES (previously no), make sure to import existing config on user choice
+do_err_popup: 
 ]]--
 
